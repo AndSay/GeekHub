ad55021559e0ed0320045fac928e02828e98cd92
AndSee <44134618+AndSay@users.noreply.github.com>
2018-11-01 18:25:47 +0200

    Add files via upload

---
+++ gh-js-tasks-master/task/task10.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/**
+ * cAPS lOCK
+ *
+ * зАЧЕМ НУЖНА КЛАВИША cAPS lOCK?
+ * Caps Lock — клавиша компьютерной клавиатуры, предназначенная для автоматической (постоянной) смены регистра
+ * букв со строчных на прописные. Будучи случайно нажатой, она приводит к последствиям вроде первого абзаца в
+ * условии этой задачи.
+ *
+ * Будем считать, что слово набрано с ошибочно нажатой клавишей Caps Lock, если:
+ * - либо оно полностью состоит из прописных букв;
+ * - либо прописными являются все его буквы, кроме первой.
+ *
+ * В таком случае, нужно автоматически поменять регистр всех букв на противоположный. Например,
+ * регистр букв слов «hELLO», «HTTP», «z» должен быть изменен.
+ * Напишите программу, которая применяет описанное выше правило или оставляет слово без изменения, если оно не применимо.
+ *
+ * Входные данные
+ * записано слово, состоящее из прописных или строчных букв латинского алфавита. Длина слова — от 1 до 100 символов включительно.
+ *
+ * Выходные данные
+ * Выведите результат обработки данного слова.
+ */
+
+var capsLockTests = [
+    {
+        parameters: ["cAPS"],
+        expectedResult: "Caps"
+    },
+    {
+        parameters: ["Lock"],
+        expectedResult: "Lock"
+    },
+    {
+        parameters: ["wHY DO wE NEED cAPS lOCK?"],
+        expectedResult: "Why do We need Caps Lock?"
+    },
+    {
+        parameters: ["FuNkY iS nOt CaPs!"],
+        expectedResult: "FuNkY Is nOt CaPs!"
+    }
+];
+
+
+function capsLock(str) {
+    var Lowstr=str.toLowerCase()
+    var UpSTR=str.toUpperCase()
+    var retstr="";
+    for(var i=0;i<str.length;i++){
+        if (str[i]==Lowstr[i]){
+            retstr+=UpSTR[i];
+        }else{
+            retstr+=Lowstr[i];
+        }
+    }
+    for( i=1;i<str.length;i++) {
+        if (retstr[i] != UpSTR[i])
+            return retstr;
+    }
+    else{
+        return str;
+    }
+}
+
+
+tasks.push({
+    title: "cAPS lOCK",
+    solution: capsLock,
+    tests: capsLockTests
+});
