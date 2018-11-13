1b2204c3bad3f77ddc18bcefd4c52bc3a824799c
AndSee <44134618+AndSay@users.noreply.github.com>
2018-11-13 08:19:47 +0200

    Update water.js

--- hm 4/water.js
+++ hm 4/water.js
@@ -1,23 +1,29 @@
-let j=10;
 var array=[0,10,10,0,5,7,2,0];
-for (let i=0;i<+j+1;i++) {                            ///генерация рандомного масива чисел
+  
+///генерация рандомного масива чисел
+let j=10;
+for (let i=0;i<+j+1;i++) {                          
     array[i]=Math.round(Math.random()*15);
 }
 console.log(array);
-var iw=0;   
-while(array.length>2){                                            /// количество води
+
+ /// количество води
+var iw=0;
+
+///запус подсчета
+while(array.length>2){                                           
 	array.forEach(water); 
-}                                  ///запус подсчета
+}                                  
 console.log(iw);
 
 
 function water(x,i,arr) {
-   debugger
+ //  debugger
     let sarr=arr.slice();
     sarr.sort(function(a, b) {
 		return b - a;
 	});
-    let ind=0;                                          //индификатор состояния, при 1 начинается подсчет, при 2 заканчивается
+    let ind=0;   //индификатор состояния, при 1 начинается подсчет, при 2 заканчивается
 
     let j;
 
@@ -28,11 +34,6 @@ function water(x,i,arr) {
 		   ind+=1;
 			
         }
- //        if (arr.indexOf(sarr[sarr.length-2])>arr.indexOf(sarr[sarr.length-1])){
-  //           j=arr.indexOf(sarr[sarr.length-1]);
-//        }else{
-//             j=arr.indexOf(sarr[sarr.length-2]);
-//         }
         if(ind==2){
             continue;
         }
@@ -40,7 +41,7 @@ function water(x,i,arr) {
             if (arr[j] != sarr[0] && arr[j] != sarr[1]) {
                 iw += sarr[1] - arr[j];                           //подсчёт количеств води
             }
-            arr.splice(j,1);                                                //удаление елементов с масива
+            arr.splice(j,1);   //удаление елементов  с масива где количество воды подсчитано
             j-=1;
         }
     }
